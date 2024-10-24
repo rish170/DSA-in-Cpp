3fea80a0533e27fe7dbf1a841136ef4f009a98db
Rishab Thutheja <rishab.17tt@gmail.com>
2024-10-24 14:07:44 +0530

    Basic Structure of C++

---
+++ Basic Structure.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World";
+    cout << "Hello World 2" << endl;
+    cout << "Hello World 2" << "\n";
+
+    //Comment
+
+    int a = 1, age;
+    float b = 1.1f;
+    char c = '*';
+    bool d = true;
+    double e = 1.17;
+    cout << a;
+    cout << sizeof(a);
+
+    cout << "Enter a value: " << endl;
+    cin >> age;
+    cout << "Entered Value: " << age << endl;
+
+    return 0;
+
+}
